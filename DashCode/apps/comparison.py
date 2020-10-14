6106f84f19c4f7da15007f80570cb032ef27d72c
Manveer Singh <msaulak@gmail.com>
2020-10-14 17:19:19 +1100

    To switch to s3

--- DashCode/apps/comparison.py
+++ DashCode/apps/comparison.py
@@ -3,6 +3,7 @@ import os
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import plotly.express as px
 from app import app
 from dash.dependencies import Input, Output
 
@@ -21,8 +22,10 @@ benchmark_options = [{'label': i, 'value': i} for i in ['Top Portfolio - All Tim
 
 benchmark_options.extend([{'label': i, 'value': i} for i in df['ASX Code']])
 
-print(benchmark_options)
-
+asx_ytd_data = pd.read_csv('/home/manveer/hackathon/VangHack2020/asx-data/prices_2020.csv')
+asx_ytd_data.set_index('Date', inplace=True)
+portfolio_data = pd.read_csv('/home/manveer/hackathon/VangHack2020/asx-data/portfolioreturns.csv')
+portfolio_data.set_index('Date', inplace=True)
 
 
 layout = html.Div([html.H1('VANGUARD FANTASY ETF EXCHANGE',
@@ -40,8 +43,8 @@ layout = html.Div([html.H1('VANGUARD FANTASY ETF EXCHANGE',
 
                        html.H3('Compare'),
                        dcc.Dropdown(id='portfolio', options=[{'label': i, 'value': i} for i in
-                                                             ['Entire Portfolio', 'VAF', 'QAU', 'MOGL', "OZR", "IOO",
-                                                              'GGUS', 'RCB', 'VEU', 'IVV', 'SFY']],
+                                                             ['Entire Portfolio', 'VAF.AX', 'QAU.AX', 'MOGL.AX', "OZR.AX", "IOO.AX",
+                                                              'GGUS.AX', 'RCB.AX', 'VEU.AX', 'IVV.AX', 'SFY.AX']],
                                     value=[], multi=False,
                                     placeholder='Entire Portfolio'),
                        html.H3('against'),
@@ -68,24 +71,62 @@ app.layout = layout
 
 @app.callback(Output('graph', 'figure'), [Input('portfolio', 'value'), Input('benchmark', 'value'),
                                           Input('category', 'value')])
-def update_output(benchmark, against, value):
-    if not benchmark or not against or not value:
+def update_output(portfolio, benchmark, category):
+    if not benchmark or not portfolio or not category:
         return None
 
+    print(portfolio)
     print(benchmark)
-    print(against)
-    print(value)
-    return None
-    # sd  = start_date.split('T')[0]
-    # ed = end_date.split('T')[0]
-    # print(tickers, sd, ed)
-    # stock_df = yf.download(tickers=tickers, start=start_date.split('T')[0], end=end_date.split('T')[0], progress=False)
-    # df_close = stock_df.Close
+    print(category)
+
+    portfolio_df = None
+    if portfolio == 'Entire Portfolio':
+        portfolio_df = portfolio_data.loc[:, ['4']]
+        portfolio_df.rename(columns={'4': portfolio}, inplace=True)
+    else:
+        portfolio_df = asx_ytd_data.loc[:, [portfolio.replace('.AX', '')]]
+    print(portfolio_df)
+
+
+    benchmark_df = None
+    benchmark_column_name = ''
+
+    if benchmark == 'Top Portfolio - All Time':
+        benchmark_df = portfolio_data.loc[:, ['6']]
+        benchmark_df.rename(columns={'6': benchmark}, inplace=True)
+    elif benchmark == 'Top Portfolio - Last Year':
+        benchmark_df = portfolio_data.loc[:, ['7']]
+        benchmark_df.rename(columns={'7': benchmark}, inplace=True)
+    elif benchmark ==  'Top Portfolio - Last Quarter':
+        benchmark_df = portfolio_data.loc[:, ['8']]
+        benchmark_df.rename(columns={'8': benchmark}, inplace=True)
+    elif benchmark == 'SPX200':
+        benchmark_df = portfolio_data.loc[:, ['9']]
+        benchmark_df.rename(columns={'9': benchmark}, inplace=True)
+    else:
+        benchmark_df = asx_ytd_data.loc[:, [benchmark.replace('.AX', '')]]
+
+
+    print(benchmark_df)
+
+    # export_dataframe = pd.concat([portfolio_df, benchmark_df], axis=1)
+    export_dataframe = portfolio_df.join(benchmark_df, how='outer')
+    # export_dataframe = export_dataframe.melt(id_vars='date', value_vars=['sessions', 'cost'])
+    export_dataframe = export_dataframe.reset_index()
+
+    print(export_dataframe)
+
+    # if category != 'YTD%':
+    #     return None
     #
-    # fig = px.line(df_close, x=df_close.index, y=[tickers], title='ETF Performance',
-    #               labels={'value': 'Price'})  # .for_each_trace(lambda t: t.update(name = t.name.replace('.AX','')))
+    # # Get portfolio data
     #
-    # fig.update_layout(legend_title_text='Ticker', hovermode='x')
-    # fig.update_traces(hovertemplate="Price: %{y} <br>Date: %{x}")
     #
-    # return fig
+    #
+    fig = px.line(export_dataframe, x='Date', y=[portfolio.replace('.AX', ''), benchmark.replace('.AX', '')], title='ETF Performance',
+                  labels={'value': 'Price'})
+
+    fig.update_layout(legend_title_text='Ticker', hovermode='x')
+    fig.update_traces(hovertemplate="Price: %{y} <br>Date: %{x}")
+
+    return fig
